e54d46eaf74272a0a9a5c098a9d869a057d4b035
FelipeLeonardo98 <felipeferrazleonardo98@gmail.com>
2020-10-11 12:26:19 -0300

    Update modified - 200

--- src/controllers/districtController.js
+++ src/controllers/districtController.js
@@ -39,23 +39,23 @@ exports.SelectAll = (req, res, next) => {
 // "Update" method
 exports.Update = (req, res, next) => {
     const id = req.params.id;
-    const nm_district = req.params.nm_district;
-    const ds_zone = req.params.ds_zone;
+    const newDistrict = req.body.nm_district;
+    const newZone = req.body.ds_zone;
 
     District.findByPk(id)
         .then(district => {
             if (district) {
                 district.update({
-                    nm_district: nm_district,
-                    ds_zone: ds_zone
+                    nm_district: newDistrict,
+                    ds_zone: newZone
                 },
                     { where: { id_district: id } }
 
                 ).then(() => {
-                    res.status(status.OK).send(`User with id ${id} updated with success`)
+                    res.status(status.OK).send(`District with id ${id} updated with success`)
                 }).catch(error => next(`Ops Attention! Look the following error: ${error}`))
             } else {
-                res.status(status.NOT_FOUND).send(`Sorry, user with id ${id} not found!`);
+                res.status(status.NOT_FOUND).send(`Sorry, district with id ${id} not found!`);
             }
         }).catch(error => next(`Ops Attention! Look the following error: ${error}`));
 };
